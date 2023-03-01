fb1352c2f85c54902849a1ba8ed70bb25176cd69
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-28 20:12:10 -0500

    terminada la clase tipos de datos

---
+++ 03_types.py
@@ -0,0 +1,21 @@
+
+#string
+my_name="Mikaela"
+my_name="Catherine"
+print("my_name =>",my_name)
+print(type(my_name))
+
+#integer
+my_age = 33
+print(my_age)
+print(type(my_age))
+
+#boolean
+is_single = True
+print(is_single)
+print(type(is_single))
+
+#inputs
+my_age = input("cual es tu edad?")
+print("my age => ",my_age)
+print(type(my_age))

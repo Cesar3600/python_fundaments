d51ad615192d4977f9156cc84ae44d1837b71679
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 18:19:52 -0500

    terminada la clase transformacion de tipos

---
+++ 07_transformation.py
@@ -0,0 +1,25 @@
+
+name = "Cesar"
+print(type(name))
+
+name = 12
+print(type(name))
+
+name = True
+print(type(name))
+
+print("Nicolas "+"Molina")
+print(12+23)
+print("nicolas"+"12")
+
+
+age=12
+print("My edad es " + str(age))
+
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad => ")
+print(type(age))
+age = int(age)
+age += 10
+print(f'tu edad en 10 anyos sera {age}')

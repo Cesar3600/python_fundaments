4ad9f6ffefa9ed48ce25ac478ad771b5975004d6
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-28 19:45:42 -0500

    variables

---
+++ 02_vars.py
@@ -0,0 +1,23 @@
+
+print("Hola vars")
+
+#esto es una variable
+my_name = "Cesar"
+print(my_name)
+
+#esto es una variable con un valor numerico
+my_age = 33
+print(my_age)
+
+# cambiamos el valor de una variable existente
+my_name="Mikaela"
+print("aqui cambio", my_name)
+
+
+#input 
+
+my_name = input("Cual es tu nombre? : ")
+print("usando input", my_name)
+
+my_age= input("Ingrese su edad : ")
+print("su edad es ",my_age)

52bb35ecebf1c4bf4847a87bfd748ff2f809effd
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 16:30:03 -0500

    Add task about total average for fisrt three months

---
+++ task_promedio_mensual.py
@@ -0,0 +1,14 @@
+
+
+
+budget_january = input("January's Budget: ");
+budget_february = input("February's Budget: ");
+budget_march = input("March's Budget: ");
+
+
+totalBudget = int(budget_january) + int(budget_february) + int(budget_march);
+AverageBudget = totalBudget/3;
+
+print(f"The average for the first three months is: {AverageBudget}");
+
+

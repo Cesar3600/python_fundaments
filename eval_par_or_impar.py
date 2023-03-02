88624860aa7815f6c2357979297d7ef5ba49d9f2
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 22:41:42 -0500

    desarrollado el reto evaluacion de numero par o impar

---
+++ eval_par_or_impar.py
@@ -0,0 +1,15 @@
+
+# insert a number
+ 
+num = int(input("ingresa un numero:::> "))
+
+res = num % 2
+if(res == 1):
+    print(f"the number {num} is ODD")
+elif(res == 0):
+    print(f"the number {num} is PAIR")
+
+
+
+
+

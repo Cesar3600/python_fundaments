cf5e86f4c1eefdb3417c80ce36208dc01390925a
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 20:49:23 -0500

    terminada la clase operadores de comparacion

---
+++ 09_comparators.py
@@ -0,0 +1,37 @@
+
+# >
+print(7 > 3) # devolvera True
+print(7 < 3) # devolvera False
+print(7 < 7) # devolvera False
+
+# <
+print(5 < 6) # devolvera True
+print(6 < 5) # devolvera False
+print(5 < 5) # devolvera False
+
+# >=
+print(2 >= 1) # devolvera True
+print(2 >= 3) # devolvera False
+print(2 >= 2) # devolvera True
+
+# <=
+print(1 <= 2) # devolvera True
+print(2 <= 1) # devolvera False
+print(2 <= 2) # devolvera True
+
+# ==
+print(6 == 6) # devolvera True
+print(5 == 2) # devolvera False
+
+# !=
+print(6 != 10) # devolvera True
+print(6 != 6) # devolvera False
+
+# ==
+print("Apple" == 'Apple') # devolvera True no importa el tipo de comilla
+print("Apple" == 'apple') # devolvera False la letra inicial es diferente
+print("1" == 1) # devolvera False tipo de datos diferente
+
+age = 18
+print( age >= 18 ) #true
+

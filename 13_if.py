89195cb503992aa7719ae3357a75a1cae9fd4b5d
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 22:35:08 -0500

    terminado clase de condicionales

---
+++ 13_if.py
@@ -0,0 +1,44 @@
+
+
+#IF
+
+if True:
+    print("deberia ejecutarse")
+
+if False:
+    print("Nunca se ejecuta")
+
+'''
+pet = input("cual es tu mascota favorita?")
+
+if(pet=='Perro'):
+    print("Buen gusto")
+
+if(pet=='Gato'):
+    print("Espero tengas suerte")
+
+'''
+
+
+'''
+stock = int(input("Digita el stock => "))
+
+if( stock >= 100 and stock <= 1000 ):
+    print('El stock es correcto')
+else:
+    print('el estock es incorrecto')
+'''
+pet = input("cual es tu mascota favorita?")
+
+if(pet=='Perro'):
+    print("Buen gusto")
+
+elif(pet=='Gato'):
+    print("Espero tengas suerte")
+
+elif(pet=='Pez'):
+    print("Eres lo maximo")
+else:
+    print("no tienes ninguna mascota interesante")
+
+

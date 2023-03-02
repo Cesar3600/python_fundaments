19d76a9abf5a74ed0eecb546dbc2f23f2a4fac2d
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-02 00:28:45 -0500

    terminada clase string recargados

---
+++ 14_strings.py
@@ -0,0 +1,59 @@
+
+'''
+text = 'Ella sabe programar en Python'
+print('Javascript' in text)
+print('Python' in text)
+
+if "Python" in text :
+    print("Elegiste bien")
+else:
+    print("Tambien elegiste bien")
+
+
+size = len('amor     ')
+print(size)
+
+
+text = 'Ella sabe programar en Python'
+size = len(text)
+print(size)
+'''
+print("#"*33)
+
+
+text = 'Ella sabe programar en PYTHON'
+size = len(text)
+print(size)
+
+print(text)
+print(text.upper())
+
+text = 'Ella sabe programar en PYTHON'
+print(text)
+print(text.lower())
+
+text = 'Ella sabe programar en PYTHON'
+print(text.count('a'))
+
+text = 'Ella sabe programar en PYTHON'
+print(text.swapcase())
+
+
+print(text.startswith('Ella'))
+
+
+print(text.endswith('Rust'))
+
+text = 'Ella sabe programar en PYTHON'
+print(text.replace('PYTHON','GO'))
+
+
+text_2 ='este es un titulo'
+print(text_2)
+print(text_2.capitalize())
+
+print(text_2.title())
+
+
+print("333".isdigit())
+

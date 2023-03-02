8bd3ada678422e6fd433113fc05f2cc1ad13d8c8
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-01 20:00:03 -0500

    terminada clase de operadores aritmeticos

---
+++ 08_arith.py
@@ -0,0 +1,23 @@
+print(10+10)
+print(10-5)
+print(10*2)
+print(10/2)
+
+print('hola'+ ' mundo')
+
+print('Hola' * 3)
+
+print(10/2) #5
+print(10%2) #0
+print(10/3) #3.333 inexacto
+
+print(10%3) 
+print(10/3) 
+print(10//3) 
+
+
+print(2**3) 
+
+print(2**3+3-7/1//4) 
+
+print(10/0)

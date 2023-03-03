f4e8c2bf2b635f715bc97aa12bdd6b63256c4367
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-02 23:01:07 -0500

    clase Listas finalizada

---
+++ 16_lists.py
@@ -0,0 +1,31 @@
+numbers = [1,2,3,4]
+
+print(numbers)
+print(type(numbers))
+
+
+tasks = ['make a dishes','play videogames']
+print(tasks)
+
+types = [1,True,'Hola']
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+
+text='Hola'
+# text[0] = 'W'
+
+tasks[0] ='watch platzi courses'
+print(tasks)
+
+tasks[0] ='Do the dishes'
+print(tasks)
+
+
+numbers = [1,2,3,4]
+print(numbers[:3])
+
+types = [1,True,'Hola']
+print(True in types)
+print('Hola' in types)
